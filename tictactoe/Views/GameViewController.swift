5e919d09e8200343246d2fbe3d53b169d555342f
vedad27 <vedadsose@gmail.com>
2016-06-16 23:03:04 +0200

    Added background lines to board

--- tictactoe/Views/GameViewController.swift
+++ tictactoe/Views/GameViewController.swift
@@ -40,12 +40,30 @@ class GameViewController: UIViewController, SubscriberDelegate {
     // Subscribe to app delegate
     game.subscribe(self)
     
+    // Generate fields
     let fieldViews = generateFields(3, size: Int(board.frame.width/3))
     
     for fieldView in fieldViews {
       fieldView.game = game
       board.addSubview(fieldView)
     }
+    
+    // Generate lines between fields
+    let line1 = UIView(frame: CGRect(x: Int(board.frame.width/3), y: 0, width: 1, height: Int(board.frame.height)))
+    line1.backgroundColor = UIColor.whiteColor()
+    board.addSubview(line1)
+    
+    let line2 = UIView(frame: CGRect(x: Int(board.frame.width/3)*2, y: 0, width: 1, height: Int(board.frame.height)))
+    line2.backgroundColor = UIColor.whiteColor()
+    board.addSubview(line2)
+    
+    let line3 = UIView(frame: CGRect(x: 0, y: Int(board.frame.height/3), width: Int(board.frame.width), height: 1))
+    line3.backgroundColor = UIColor.whiteColor()
+    board.addSubview(line3)
+    
+    let line4 = UIView(frame: CGRect(x: 0, y: Int(board.frame.height/3)*2, width: Int(board.frame.width), height: 1))
+    line4.backgroundColor = UIColor.whiteColor()
+    board.addSubview(line4)
   }
   
   // Generate field views
